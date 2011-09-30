c347deebe239fb5d25baf76d2dde7daf519ae5ea
MFShick <MFShick@gmail.com>
2011-09-30 05:52:11 +0000

    Added GPL compliance stuff for my code; created non-functional life counter activity

--- MTGFamiliar/src/com/mtg/fam/counter.java
+++ MTGFamiliar/src/com/mtg/fam/counter.java
@@ -1,129 +1,31 @@
+/**
+Copyright 2011 Michael Shick
+
+This file is part of MTG Familiar.
+
+MTG Familiar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MTG Familiar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MTG Familiar.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.mtg.fam;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextSwitcher;
-import android.widget.TextView;
-import android.widget.ViewSwitcher;
 
-public class counter extends Activity  implements ViewSwitcher.ViewFactory{
-
-	private TextSwitcher lifeP1;
-	private TextSwitcher poisonP1;
-	private TextSwitcher lifeP2;
-	private TextSwitcher poisonP2;
-	private Button life_p1_up;
-	private Button life_p1_dn;
-	private Button poison_p1_up;
-	private Button poison_p1_dn;
-	private Button life_p2_up;
-	private Button life_p2_dn;
-	private Button poison_p2_up;
-	private Button poison_p2_dn;
-	
-	private static int life_p1;
-	private static int poison_p1;
-	private static int life_p2;
-	private static int poison_p2;	
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.counter);
-		
-		life_p1 = 20;
-		poison_p1 = 0;
-		life_p2 = 20;
-		poison_p2 = 0;
-		
-		lifeP1 = (TextSwitcher)findViewById(R.id.life_p1);
-		poisonP1 = (TextSwitcher)findViewById(R.id.poison_p1);
-		lifeP2 = (TextSwitcher)findViewById(R.id.life_p2);
-		poisonP2 = (TextSwitcher)findViewById(R.id.poison_p2);
-		
-		lifeP1.setFactory(this);
-		poisonP1.setFactory(this);
-		lifeP2.setFactory(this);
-		poisonP2.setFactory(this);
-		
-		Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
-		Animation out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
-		lifeP1.setInAnimation(in);
-		lifeP1.setOutAnimation(out);
-		poisonP1.setInAnimation(in);
-		poisonP1.setOutAnimation(out);
-		lifeP2.setInAnimation(in);
-		lifeP2.setOutAnimation(out);
-		poisonP2.setInAnimation(in);
-		poisonP2.setOutAnimation(out);
-		
-		lifeP1.setText(""+life_p1);
-		poisonP1.setText(""+poison_p1);
-		lifeP2.setText(""+life_p2);
-		poisonP2.setText(""+poison_p2);
-		
-		life_p1_up = (Button)findViewById(R.id.life_p1_up);
-		life_p1_dn = (Button)findViewById(R.id.life_p1_dn);
-		poison_p1_up = (Button)findViewById(R.id.poison_p1_up);
-		poison_p1_dn = (Button)findViewById(R.id.poison_p1_dn);
-		life_p2_up = (Button)findViewById(R.id.life_p2_up);
-		life_p2_dn = (Button)findViewById(R.id.life_p2_dn);
-		poison_p2_up = (Button)findViewById(R.id.poison_p2_up);
-		poison_p2_dn = (Button)findViewById(R.id.poison_p2_dn);
-		
-		life_p1_up.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				lifeP1.setText((++life_p1)+"");
-			}
-		});
-		life_p1_dn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				lifeP1.setText((--life_p1)+"");
-			}
-		});
-		poison_p1_up.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				poisonP1.setText((++poison_p1)+"");
-			}
-		});
-		poison_p1_dn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				poisonP1.setText((--poison_p1)+"");
-			}
-		});
-		life_p2_up.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				lifeP2.setText((++life_p2)+"");
-			}
-		});
-		life_p2_dn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				lifeP2.setText((--life_p2)+"");
-			}
-		});
-		poison_p2_up.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				poisonP2.setText((++poison_p2)+"");
-			}
-		});
-		poison_p2_dn.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				poisonP2.setText((--poison_p2)+"");
-			}
-		});
-
-	}
-
-	public View makeView() {
-    TextView t = new TextView(this);
-    t.setGravity(Gravity.CENTER);
-    t.setTextSize(100);
-//    t.setTextColor(R.color.textcolor);
-      return t;
-	}
+public class counter extends Activity
+{
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.counter);
+    }
 }

dc007ba620c034eedf40665afad7be4c26b3d761
gelakinetic@gmail.com <gelakinetic@gmail.com@3269d5d8-89f5-7437-af3c-7001695c22c9>
2011-10-19 05:21:21 +0000

    Issue 37 - Moved timestamp update
    Fixed a warning elsewhere

--- trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/ResultListActivity.java
+++ trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/ResultListActivity.java
@@ -30,7 +30,6 @@ import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;

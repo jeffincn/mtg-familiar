e061ceea002bb2403f89e76a0bdc17b06edf42e9
gelakinetic <gelakinetic@gmail.com>
2012-09-14 02:22:20 +0000

    Let the fragmentation begin! Wait, it's the good kind of fragmentation.
    MoJhoSto is now all done in a fragment, and it you shouldn't notice a difference.

---
+++ MTGFamiliar/src/com/gelakinetic/fragments/FamiliarDialogFragment.java
@@ -0,0 +1,29 @@
+package com.gelakinetic.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+public class FamiliarDialogFragment extends SherlockDialogFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+
+	@Override
+	public void onDestroyView() {
+		if (getDialog() != null && getRetainInstance())
+			getDialog().setOnDismissListener(null);
+		super.onDestroyView();
+	}
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		//int id = savedInstanceState.getInt("id");
+		return null;
+	}
+
+}

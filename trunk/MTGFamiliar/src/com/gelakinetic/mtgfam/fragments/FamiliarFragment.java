7c9e80bc9ddf380192ed368d0eb3a80786273173
ghislain.nebra@gmail.com <ghislain.nebra@gmail.com@3269d5d8-89f5-7437-af3c-7001695c22c9>
2012-11-28 16:57:01 +0000

    onSaveInstanceState needed to be overriden into FamiliarFragment to solve a crash dealing with "Can not perform this action after onSaveInstanceState". It is an Android known bug : more info on http://code.google.com/p/android/issues/detail?id=19917

--- trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/fragments/FamiliarFragment.java
+++ trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/fragments/FamiliarFragment.java
@@ -90,6 +90,14 @@ public class FamiliarFragment extends SherlockFragment {
 	}
 
 	@Override
+    public void onSaveInstanceState(Bundle outState) {
+    	//first saving my state, so the bundle wont be empty.
+    	//http://code.google.com/p/android/issues/detail?id=19917
+    	outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
+    	super.onSaveInstanceState(outState);
+    }
+	
+	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 

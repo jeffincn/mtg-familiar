d329cae2c017ab7a9b0e035dd060866cb28239d6
gelakinetic@gmail.com <gelakinetic@gmail.com@3269d5d8-89f5-7437-af3c-7001695c22c9>
2012-12-29 01:36:09 +0000

    Added Judge's Corner skeleton (tabs for MTR, IPG, Deck Counter)

---
+++ trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/fragments/MTRFragment.java
@@ -0,0 +1,23 @@
+package com.gelakinetic.mtgfam.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MTRFragment extends FamiliarFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+
+		LinearLayout ll = new LinearLayout(this.getActivity());
+		TextView tv = new TextView(this.getActivity());
+		tv.setText("MTR");
+		ll.addView(tv);
+		return ll;
+	}
+}

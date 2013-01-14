524f2eec54ac9bd98f6f6ce5a415a76a7f7fac86
gelakinetic@gmail.com <gelakinetic@gmail.com@3269d5d8-89f5-7437-af3c-7001695c22c9>
2013-01-14 00:30:32 +0000

    Goggles now stores images locally, and then deletes them when done, regardless of phone.

--- trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/fragments/FamiliarFragment.java
+++ trunk/MTGFamiliar/src/com/gelakinetic/mtgfam/fragments/FamiliarFragment.java
@@ -1,13 +1,17 @@
 package com.gelakinetic.mtgfam.fragments;
 
+import java.io.File;
 import java.io.IOException;
 
 import android.app.Instrumentation;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -38,6 +42,8 @@ public class FamiliarFragment extends SherlockFragment {
 
 	public static final int 	ACTIVITY_CAMERA_GOGGLES		= 1;
 	protected static final int GOGGLES_ANALYSIS = 1;
+	private static final String TMP_IMG_FILENAME = "mtgfam-tmp.jpg";
+	File tmp_img_file;
 
 	public FamiliarFragment() {
 		/* http://developer.android.com/reference/android/app/Fragment.html
@@ -48,11 +54,11 @@ public class FamiliarFragment extends SherlockFragment {
 		 * will occur in some cases during state restore. 
 		 */
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		try {
 			mDbHelper = new CardDbAdapter(this.getMainActivity());
 		} catch (FamiliarDbException e) {
@@ -69,7 +75,7 @@ public class FamiliarFragment extends SherlockFragment {
 		progDialog.setMessage(getString(R.string.goggles_photo_analysis));
 		progDialog.setIndeterminate(true);
 		progDialog.setCancelable(true);
-		
+
 		if(mGogglesTask != null) {
 			progDialog.show();
 		}
@@ -78,7 +84,6 @@ public class FamiliarFragment extends SherlockFragment {
 
 	@Override
 	public void onDestroyView() {
-		// TODO Auto-generated method stub
 		if(progDialog.isShowing()) {
 			progDialog.cancel();
 		}
@@ -90,7 +95,7 @@ public class FamiliarFragment extends SherlockFragment {
 		super.onPause();
 		removeDialog();
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
@@ -109,36 +114,36 @@ public class FamiliarFragment extends SherlockFragment {
 	}
 
 	@Override
-    public void onSaveInstanceState(Bundle outState) {
-    	//first saving my state, so the bundle wont be empty.
-    	//http://code.google.com/p/android/issues/detail?id=19917
-    	outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
-    	super.onSaveInstanceState(outState);
-    }
-	
+	public void onSaveInstanceState(Bundle outState) {
+		//first saving my state, so the bundle wont be empty.
+		//http://code.google.com/p/android/issues/detail?id=19917
+		outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
+		super.onSaveInstanceState(outState);
+	}
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 
 		menu.clear();
 		menu.add(R.string.name_search_hint).setIcon(R.drawable.menu_search)
-				.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+		.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				new Thread(new Runnable() {
 					@Override
-					public boolean onMenuItemClick(MenuItem item) {
-						new Thread(new Runnable() {
-							@Override
-							public void run() {
-								try{
-									new Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_SEARCH);
-								}
-								catch(java.lang.SecurityException e){
-									//apparently this can inject an event into another app if the user switches fast enough
-								}
-							}
-						}).start();
-						return true;
+					public void run() {
+						try{
+							new Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_SEARCH);
+						}
+						catch(java.lang.SecurityException e){
+							//apparently this can inject an event into another app if the user switches fast enough
+						}
 					}
-				}).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+				}).start();
+				return true;
+			}
+		}).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 	}
 
 	public MainActivity getMainActivity() {
@@ -153,7 +158,7 @@ public class FamiliarFragment extends SherlockFragment {
 	public boolean onInterceptSearchKey() {
 		return false;
 	}
-	
+
 	protected void startNewFragment(FamiliarFragment frag, Bundle args){
 		startNewFragment(frag, args, true);
 	}
@@ -165,7 +170,7 @@ public class FamiliarFragment extends SherlockFragment {
 		if (allowBackStack) {
 			fragmentTransaction.addToBackStack(null);
 		}
-		
+
 		fragmentTransaction.replace(R.id.frag_view, frag);
 		fragmentTransaction.commit();
 		this.getMainActivity().hideKeyboard();
@@ -184,33 +189,35 @@ public class FamiliarFragment extends SherlockFragment {
 			// eat it
 		}
 	}
-	
-    public FragmentManager getSupportFragmentManager() {
-		// TODO Auto-generated method stub
+
+	public FragmentManager getSupportFragmentManager() {
 		return this.getFragmentManager();
 	}
 
 	protected void takePictureAndSearchGoogleGogglesIntent() {
-    	Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, ACTIVITY_CAMERA_GOGGLES);
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-    }
+		tmp_img_file = new File (Environment.getExternalStorageDirectory(), TMP_IMG_FILENAME);
+		takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tmp_img_file));
+
+		startActivityForResult(takePictureIntent, ACTIVITY_CAMERA_GOGGLES);
+	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
-			case ACTIVITY_CAMERA_GOGGLES:
-				switch (resultCode) {
-				case android.app.Activity.RESULT_OK:
-					mGogglesTask = new GoogleGogglesTask();
-					mGogglesTask.execute(data);
-					return;
-				}
+		case ACTIVITY_CAMERA_GOGGLES:
+			switch (resultCode) {
+			case android.app.Activity.RESULT_OK:
+				mGogglesTask = new GoogleGogglesTask();
+				mGogglesTask.execute(data);
 				return;
+			}
+			return;
 		}
-		
+
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-	
+
 	public class GoogleGogglesTask extends AsyncTask<Intent, Void, Void> {
 
 		private String	cardName;
@@ -226,14 +233,32 @@ public class FamiliarFragment extends SherlockFragment {
 		@Override
 		protected Void doInBackground(Intent... params) {
 			try {
-			    Bitmap mImageBitmap = (Bitmap) params[0].getExtras().get("data");
-					
-			    cardName = "";
-			    try {
-			    	cardName = GoogleGoggles.StartCardSearch(mImageBitmap, getActivity(), mDbHelper);
-
-				} catch (IOException e) {
+				if(tmp_img_file == null) {
+					return null;
 				}
+
+				// First decode with inJustDecodeBounds=true to check dimensions
+				final BitmapFactory.Options options = new BitmapFactory.Options();
+				options.inJustDecodeBounds = true;
+				options.inPurgeable = true;
+				BitmapFactory.decodeFile(tmp_img_file.getAbsolutePath(), options);
+
+				// Calculate inSampleSize
+				options.inSampleSize = options.outWidth / 204;
+
+				// Decode bitmap with inSampleSize set
+				options.inJustDecodeBounds = false;
+				Bitmap mImageBitmap = BitmapFactory.decodeFile(tmp_img_file.getAbsolutePath(), options);
+
+				cardName = "";
+				try {
+					cardName = GoogleGoggles.StartCardSearch(mImageBitmap, getActivity(), mDbHelper);
+				} catch (IOException e) {
+					cardName = null;
+				}
+				mImageBitmap.recycle();
+				tmp_img_file.delete();
+				System.gc(); // take out the trash
 			} catch (Exception e) {
 				cardName = null;
 			}
@@ -261,8 +286,8 @@ public class FamiliarFragment extends SherlockFragment {
 		}
 	}
 
-    protected void onGoogleGogglesSuccess(String cardName) {
-    	// this method must be overridden by each class calling takePictureAndSearchGoogleGogglesIntent
-    	mGogglesTask = null;
+	protected void onGoogleGogglesSuccess(String cardName) {
+		// this method must be overridden by each class calling takePictureAndSearchGoogleGogglesIntent
+		mGogglesTask = null;
 	}
 }

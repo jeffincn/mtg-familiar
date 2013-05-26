3a36628abf0e9c4fea61f776136ac35dca766796
DiabloMarcus <DiabloMarcus@3269d5d8-89f5-7437-af3c-7001695c22c9>
2013-05-26 14:40:29 +0000

    Getting rid of testing for now until I can figure out how to do it on Android -mak

--- branches/diablomarcus/mtgfamtest-test/gen/com/gelakinetic/mtgfam/test/clicklisteners/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gelakinetic.mtgfam.test.clicklisteners;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
